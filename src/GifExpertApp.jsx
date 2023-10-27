2fb540973a6b050713d70b2de86047b27f4d84c5
Santiago Hernandez Molina <santiagohm@pop-os.localdomain>
2023-10-27 16:09:41 -0500

    vercel config

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -4,29 +4,25 @@ import { AddCategory,GifGrid } from "./components";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['javascript']);
-
+    const [categories, setCategories] = useState(['Linux']);
+    
     const addCategory = (newCategory) => {
-
         if (!categories.includes(newCategory)) {
             setCategories((cat) => [ newCategory,...cat ]);
         }
     }
 
     return (
-
         <>
-            <h1>Gif Expert App</h1>
-
-            <AddCategory
-                onNewCategory={(value) => addCategory(value)}
-            />
-
-            {categories.map((category) => (
-                <GifGrid
-                    key={category}
-                    category={category} />
-            ))}
+        <h1>Gif Expert App</h1>
+        <AddCategory
+        onNewCategory={(value) => addCategory(value)}
+        />
+        {categories.map((category) => (
+            <GifGrid
+            key={category}
+            category={category} />
+        ))}
         </>
     );
 }
